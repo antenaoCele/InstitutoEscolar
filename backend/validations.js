689d8961ef1d449e58fa25bc9929cb0945db20b8
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-02-11 16:07:21 -0300

    Se corrigieron validaciones

--- backend/validations.js
+++ backend/validations.js
@@ -35,7 +35,7 @@ export const validatePayments = [
     .isInt({ min: 1 })
     .withMessage("El alumno debe ser válido.")
     .custom(async (value) => {
-      const [rows] = await db.execute("SELECT id FROM alumnos WHERE id = ?", [
+      const [rows] = await db.execute("SELECT id FROM students WHERE id = ?", [
         value,
       ]);
       if (rows.length === 0) {
@@ -44,13 +44,13 @@ export const validatePayments = [
       return true;
     }),
 
-  body("payment_day")
+  body("payment_date")
     .notEmpty()
     .withMessage("La fecha de pago es obligatoria.")
     .isISO8601()
     .withMessage("La fecha debe tener formato AAAA-MM-DD."),
 
-  body("mount")
+  body("amount")
     .notEmpty()
     .withMessage("El monto es obligatorio.")
     .isDecimal({ decimal_digits: "1,2" })
