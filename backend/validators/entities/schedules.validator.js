52bec915f3e51dbf12cc8f59520d69bb829e861f
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-03-26 21:44:12 -0300

    Se agregaron validaciones y se eliminaron importaciones innecesarias

--- backend/validators/entities/schedules.validator.js
+++ backend/validators/entities/schedules.validator.js
@@ -1,28 +1,31 @@
 import {
   validateForeignId,
   validateScheduleOverlap,
-  validateScheduleOverlapOnUpdate,
 } from "../rules/database.rules.js";
 import {
+  validateDay,
+  validateClassroom,
   validateFKFormat,
   validateHour,
-  validateHourRange,
+  validateStartTime,
 } from "../rules/format.rules.js";
 
 export const validateSchedules = [
   ...validateFKFormat("teacher_id"),
   ...validateForeignId("teacher_id", "teachers"),
+  ...validateDay("day"),
+  ...validateClassroom("classroom"),
   ...validateHour("start_time"),
-  ...validateHour("end_time"),
-  ...validateHourRange("start_time", "end_time"),
+  ...validateStartTime("start_time"),
   ...validateScheduleOverlap("schedules"),
 ];
 
 export const validateEditSchedules = [
   ...validateFKFormat("teacher_id", true),
-  ...validateForeignId("teacher_id", "teachers", true),
+  ...validateForeignId("teacher_id", "teachers"),
+  ...validateDay("day", true),
+  ...validateClassroom("classroom", true),
   ...validateHour("start_time", true),
-  ...validateHour("end_time", true),
-  ...validateHourRange("start_time", "end_time", true),
-  ...validateScheduleOverlapOnUpdate("schedules"),
+  ...validateStartTime("start_time", true),
+  ...validateScheduleOverlap("schedules"),
 ];
