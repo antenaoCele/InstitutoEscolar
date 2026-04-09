cf2d16b6095c74b8971de7a533fb2f1b68b4d3a7
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-04-08 22:13:56 -0300

    Se creó el archivo Label.jsx

---
+++ frontend/src/components/form/switch/Label.jsx
@@ -0,0 +1,19 @@
+import { twMerge } from "tailwind-merge";
+
+export default function Label({
+  htmlFor,
+  children,
+  className,
+}) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={twMerge(
+        "mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400",
+        className
+      )}
+    >
+      {children}
+    </label>
+  );
+}
