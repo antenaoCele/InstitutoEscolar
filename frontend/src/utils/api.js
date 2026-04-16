c9826ccea82782f52adaf962216a2edb5522427a
Cele Antenao <cele.antenao@gmail.com>
2026-04-16 11:33:20 -0300

    Agrego ruta base en api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,14 @@
+// export const getAuthHeaders = () => {
+//   const token = localStorage.getItem("token");
+
+//   return {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer ${token}`,
+//   };
+// };
+
+import axios from "axios";
+
 export const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
 
@@ -6,3 +17,18 @@ export const getAuthHeaders = () => {
     Authorization: `Bearer ${token}`,
   };
 };
+
+const api = axios.create({
+  baseURL: "http://localhost:3000", // RUTA BASE
+});
+
+//Antes de enviar cualquier peticion se agregan los headers
+api.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    ...getAuthHeaders(),
+  };
+  return config;
+});
+
+export default api;
