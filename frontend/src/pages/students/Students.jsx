a8044c4e15fd1f880d4229404e4919af564ce88d
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-04-22 13:21:25 -0300

    Se cambio el fetch y se agregaron datos faltantes a la tabla

--- frontend/src/pages/students/Students.jsx
+++ frontend/src/pages/students/Students.jsx
@@ -1,31 +1,37 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/Auth.jsx";
+import { useLocation } from "react-router-dom";
 
 export function Students() {
   const { fetchAuth } = useAuth();
   const [students, setStudents] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
     const fetchStudents = async () => {
       try {
-        const response = await fetchAuth("http://localhost:3000/students");
+        const params = new URLSearchParams(location.search);
+        const status = params.get("status") || "all";
+
+        const response = await fetchAuth(
+          `http://localhost:3000/students?status=${status}`
+        );
+
         const data = await response.json();
 
         if (!response.ok || !data.success) {
-          console.log("Error:", data.message || data.error);
           setStudents([]);
           return;
         }
 
         setStudents(data.data || []);
-      } catch (error) {
-        console.log("Error de conexión:", error);
+      } catch {
         setStudents([]);
       }
     };
 
     fetchStudents();
-  }, [fetchAuth]);
+  }, [fetchAuth, location.search]);
 
   return (
     <article>
@@ -40,24 +46,28 @@ export function Students() {
             <th>DNI</th>
             <th>Escuela</th>
             <th>Fecha de nacimiento</th>
+            <th>Nivel</th>
+            <th>Grado</th>
           </tr>
         </thead>
 
         <tbody>
           {students.length > 0 ? (
             students.map((s) => (
-              <tr key={s.id}>
-                <td>{s.id}</td>
+              <tr key={s.student_id}>
+                <td>{s.student_id}</td>
                 <td>{s.last_name}</td>
                 <td>{s.first_name}</td>
                 <td>{s.dni}</td>
-                <td>{s.school}</td>
-                <td>{new Date(s.birth_date).toISOString().slice(0, 10)}</td>
+                <td>{s.school || "-"}</td>
+                <td>{s.birth_date ? new Date(s.birth_date).toLocaleDateString("es-AR"): "-"}</td>
+                <td>{s.level}</td>
+                <td>{s.grade}</td>
               </tr>
             ))
           ) : (
             <tr>
-              <td colSpan="5">No hay estudiantes cargados</td>
+              <td colSpan="6">No hay estudiantes</td>
             </tr>
           )}
         </tbody>
