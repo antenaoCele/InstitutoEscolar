1780a719a8177931e3ab022d24ea478c71699c9e
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-04-23 12:38:50 -0300

    Se importó useAuth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./context/Auth.jsx";
 
 import Login from "./pages/login/Login.jsx";
 import UserProfiles from "./pages/me/UserProfiles.jsx";
 import { Users } from "./pages/users/AllUsers.jsx";
 import { Layout } from "./Layout.jsx";
-import PrivateRoute from "./routes/PrivateRoute.jsx"
+import PrivateRoute from "./routes/PrivateRoute.jsx";
 import { Teachers } from "./pages/teachers/Teachers.jsx";
 import { Students } from "./pages/students/Students.jsx";
 import { Tutors } from "./pages/tutors/Tutors.jsx";
 import { Schedules } from "./pages/schedules/Schedules.jsx";
 
 export default function App() {
-  const token = localStorage.getItem("token");
+  const { isAuthenticated } = useAuth();
 
   return (
     <BrowserRouter>
@@ -21,7 +22,7 @@ export default function App() {
         <Route
           path="/"
           element={
-            token ? (
+            isAuthenticated ? (
               <Navigate to="/me" replace />
             ) : (
               <Navigate to="/login" replace />
@@ -33,23 +34,23 @@ export default function App() {
         <Route
           path="/login"
           element={
-            token ? <Navigate to="/me" replace /> : <Login />
+            isAuthenticated ? <Navigate to="/me" replace /> : <Login />
           }
         />
 
         {/* Rutas protegidas */}
         <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Layout />}>
-            <Route path="me" element={<UserProfiles />} />
-            <Route path="users" element={<Users />} />
-            <Route path="students" element={<Students />} />
-            <Route path="teachers" element={<Teachers />} />
-            <Route path="tutors" element={<Tutors />} />
-            <Route path="schedules" element={<Schedules />} />
+          <Route element={<Layout />}>
+            <Route path="/me" element={<UserProfiles />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/teachers" element={<Teachers />} />
+            <Route path="/tutors" element={<Tutors />} />
+            <Route path="/schedules" element={<Schedules />} />
           </Route>
         </Route>
 
-        {/* Cualquier otra cosa */}
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
 
       </Routes>
