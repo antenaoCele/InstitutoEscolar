46261eeaabd5abd0580287429266059673c68ba7
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-05-23 10:53:13 -0300

    Se refactorizó métodos create y update en tutorService para explicitar la estructura del request body

--- frontend/src/services/tutor.service.js
+++ frontend/src/services/tutor.service.js
@@ -2,8 +2,24 @@ import api from "../utils/api";
 
 export const tutorService = {
   getAll: (params = {}) => api.get("/tutors", { params }),
+
   getById: (id) => api.get(`/tutors/${id}`),
-  create: (data) => api.post("/tutors", data),
-  update: (id, data) => api.put(`/tutors/${id}`, data),
+
+  create: (data) =>
+    api.post("/tutors", {
+      first_name: data.first_name,
+      last_name: data.last_name,
+      dni: data.dni,
+      phone: data.phone,
+    }),
+
+  update: (id, data) =>
+    api.put(`/tutors/${id}`, {
+      first_name: data.first_name,
+      last_name: data.last_name,
+      dni: data.dni,
+      phone: data.phone,
+    }),
+
   delete: (id) => api.delete(`/tutors/${id}`),
 };
