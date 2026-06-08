e68ec156bf17a551da4e928df13a430ea5402e09
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-06-08 01:24:18 -0300

    Agrega validaciones para el campo de asignatura en la validación de horarios

--- backend/validators/entities/schedules.validator.js
+++ backend/validators/entities/schedules.validator.js
@@ -14,9 +14,15 @@ import {
 export const validateSchedules = [
   ...validateFKFormat("teacher_id"),
   ...validateForeignId("teacher_id", "teachers"),
+
+  ...validateFKFormat("subject_id"),
+  ...validateForeignId("subject_id", "subjects"),
+
   ...validateHour("start_time"),
   ...validateStartTime("start_time"),
+
   ...validateScheduleConflict("schedules"),
+
   ...validateDay("day"),
   ...validateClassroom("classroom"),
 ];
@@ -24,9 +30,15 @@ export const validateSchedules = [
 export const validateEditSchedules = [
   ...validateFKFormat("teacher_id", true),
   ...validateForeignId("teacher_id", "teachers", true),
+
+  ...validateFKFormat("subject_id", true),
+  ...validateForeignId("subject_id", "subjects", true),
+
   ...validateHour("start_time", true),
   ...validateStartTime("start_time", true),
+
   ...validateScheduleConflict("schedules"),
+
   ...validateDay("day", true),
   ...validateClassroom("classroom", true),
 ];
