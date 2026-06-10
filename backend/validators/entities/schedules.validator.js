9d83d9f832eb2d6333b7b749f814949d64326b9f
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-06-10 19:54:13 -0300

    Elimina validaciones de asignatura en la validación de horarios.

--- backend/validators/entities/schedules.validator.js
+++ backend/validators/entities/schedules.validator.js
@@ -15,9 +15,6 @@ export const validateSchedules = [
   ...validateFKFormat("teacher_id"),
   ...validateForeignId("teacher_id", "teachers"),
 
-  ...validateFKFormat("subject_id"),
-  ...validateForeignId("subject_id", "subjects"),
-
   ...validateHour("start_time"),
   ...validateStartTime("start_time"),
 
@@ -30,9 +27,6 @@ export const validateSchedules = [
 export const validateEditSchedules = [
   ...validateFKFormat("teacher_id", true),
   ...validateForeignId("teacher_id", "teachers", true),
-
-  ...validateFKFormat("subject_id", true),
-  ...validateForeignId("subject_id", "subjects", true),
 
   ...validateHour("start_time", true),
   ...validateStartTime("start_time", true),
