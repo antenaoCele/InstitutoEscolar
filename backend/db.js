f44bb8b3147a720a8d03cae8b54dcf2c9ed0c55c
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-06-10 19:53:04 -0300

    Cambia la conexión a la base de datos para usar un pool de conexiones, mejorando la gestión de múltiples conexiones.

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,14 @@ import mysql from "mysql2/promise";
 export let db;
 
 export async function connectDB() {
-    db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-    })
+  db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
 }
