647655cbbf0772e5edee874a5c6f90b86aefd07b
Cele Antenao <cele.antenao@gmail.com>
2026-06-16 16:17:55 -0300

    Instrucciones en inicio

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,12 @@ export const Home = () => {
   return (
     <article>
       <h1>ARREGLAR EL INICIO!!!</h1>
+      <h6> - Corregir que en planes no puedo editar nunca una fecha de fin</h6>
+      <h6>
+        - Ver en donde vamos a establecer la relacion entre alumno - docente
+      </h6>
+      <h6>- Ver en donde le asignamos un plan al alumno</h6>
+      <h6>- Ver en donde manejamos los pagos del alumno</h6>
     </article>
   );
 };
