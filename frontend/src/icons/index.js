a4beac1e322d0ae17133c1a15299e72a8a582f8b
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-06-29 19:43:11 -0300

    Agrega nuevo ícono de vista y actualiza la exportación en index.js

--- frontend/src/icons/index.js
+++ frontend/src/icons/index.js
@@ -58,6 +58,7 @@ import CreateIcon from "./icons8-crear-48.png";
 import MoreInfoIcon from "./icons8-info-50.png";
 import BackIcon from "./icons8-izquierda-negro-2-50.png";
 import CloseSessionIcon from "./icons8-cerrar.png";
+import ViewIcon from "./icons8-ver-50 (1).png";
 
 export {
   ErrorHexaIcon,
@@ -120,4 +121,5 @@ export {
   MoreInfoIcon,
   BackIcon,
   CloseSessionIcon,
+  ViewIcon,
 };
