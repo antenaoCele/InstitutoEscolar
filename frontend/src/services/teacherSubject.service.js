71e0ec3a982523a919b09dfa25a2717278c54d53
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-07-12 15:20:17 -0300

    Elimina el controlador, las rutas y las validaciones para los temas de docentes

--- frontend/src/services/teacherSubject.service.js
+++
@@ -1,9 +0,0 @@
-import api from "../utils/api";
-
-export const teacherSubjectService = {
-  getAll: () => api.get("/teacher_subjects"),
-  getById: (id) => api.get(`/teacher_subjects/${id}`),
-  create: (data) => api.post("/teacher_subjects", data),
-  update: (id, data) => api.put(`/teacher_subjects/${id}`, data),
-  delete: (id) => api.delete(`/teacher_subjects/${id}`),
-};
