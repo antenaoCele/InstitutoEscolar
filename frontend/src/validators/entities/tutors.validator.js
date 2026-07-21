6ec49f2cec1efc9570337b310566a321479b34c2
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-07-20 21:21:48 -0300

    Añade validador para el formulario de tutores con mensajes de error específicos

---
+++ frontend/src/validators/entities/tutors.validator.js
@@ -0,0 +1,40 @@
+import {
+  isRequired,
+  isValidPersonName,
+  isValidDNI,
+  isValidPhone,
+} from "../rules/formatRules";
+
+export const validateTutorForm = ({ first_name, last_name, dni, phone }) => {
+  const errors = {};
+
+  // Nombre
+  if (isRequired(first_name)) {
+    errors.first_name = "Campo obligatorio.";
+  } else if (!isValidPersonName(first_name)) {
+    errors.first_name = "Ingrese un nombre válido.";
+  }
+
+  // Apellido
+  if (isRequired(last_name)) {
+    errors.last_name = "Campo obligatorio.";
+  } else if (!isValidPersonName(last_name)) {
+    errors.last_name = "Ingrese un apellido válido.";
+  }
+
+  // DNI
+  if (isRequired(dni)) {
+    errors.dni = "Campo obligatorio.";
+  } else if (!isValidDNI(dni)) {
+    errors.dni = "Ingrese un DNI válido.";
+  }
+
+  // Teléfono
+  if (isRequired(phone)) {
+    errors.phone = "Campo obligatorio.";
+  } else if (!isValidPhone(phone)) {
+    errors.phone = "Ingrese un teléfono válido.";
+  }
+
+  return errors;
+};
