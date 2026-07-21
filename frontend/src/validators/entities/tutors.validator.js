723ef9e535283fa5ebaa7bf88902e910a69c5782
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-07-21 14:16:13 -0300

    Corrige validación de nombres y apellidos en validateTutorForm

--- frontend/src/validators/entities/tutors.validator.js
+++ frontend/src/validators/entities/tutors.validator.js
@@ -5,35 +5,31 @@ import {
   isValidPhone,
 } from "../rules/formatRules";
 
-export const validateTutorForm = ({ first_name, last_name, dni, phone }) => {
+export const validateTutorForm = ({ firstName, lastName, dni, phone }) => {
   const errors = {};
 
-  // Nombre
-  if (isRequired(first_name)) {
+  if (isRequired(firstName)) {
     errors.first_name = "Campo obligatorio.";
-  } else if (!isValidPersonName(first_name)) {
-    errors.first_name = "Ingrese un nombre válido.";
+  } else if (!isValidPersonName(firstName)) {
+    errors.first_name = "Formato inválido.";
   }
 
-  // Apellido
-  if (isRequired(last_name)) {
+  if (isRequired(lastName)) {
     errors.last_name = "Campo obligatorio.";
-  } else if (!isValidPersonName(last_name)) {
-    errors.last_name = "Ingrese un apellido válido.";
+  } else if (!isValidPersonName(lastName)) {
+    errors.last_name = "Formato inválido.";
   }
 
-  // DNI
   if (isRequired(dni)) {
     errors.dni = "Campo obligatorio.";
   } else if (!isValidDNI(dni)) {
-    errors.dni = "Ingrese un DNI válido.";
+    errors.dni = "Formato inválido.";
   }
 
-  // Teléfono
   if (isRequired(phone)) {
     errors.phone = "Campo obligatorio.";
   } else if (!isValidPhone(phone)) {
-    errors.phone = "Ingrese un teléfono válido.";
+    errors.phone = "Formato inválido.";
   }
 
   return errors;
