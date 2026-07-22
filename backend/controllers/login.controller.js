765491dcc82880ec13dc096767f259461081bfa7
Cele Antenao <cele.antenao@gmail.com>
2026-07-22 01:00:20 -0300

    Edito hora de expiracion

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -43,7 +43,7 @@ export const login = async (req, res) => {
     };
 
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1m",
+      expiresIn: "4h",
     });
 
     res.json({
