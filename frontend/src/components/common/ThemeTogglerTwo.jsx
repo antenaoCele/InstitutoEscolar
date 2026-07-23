37c6fbe595978d1adc5485c3ec9af0ba3979715d
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-07-23 04:52:41 -0300

    Reemplaza el icono de sol por SunFatIcon en el componente ThemeTogglerTwo

--- frontend/src/components/common/ThemeTogglerTwo.jsx
+++ frontend/src/components/common/ThemeTogglerTwo.jsx
@@ -1,4 +1,5 @@
 import { useTheme } from "../../context/ThemeContext";
+import { SunFatIcon } from "../../icons";
 
 export default function ThemeTogglerTwo() {
   const { theme, toggleTheme } = useTheme();
@@ -22,16 +23,11 @@ export default function ThemeTogglerTwo() {
         `}
       />
 
-      {/* ICONO SOL */}
-      <svg
+      <SunFatIcon
         className={`absolute left-2 w-4 h-4 transition-opacity ${
           isDark ? "opacity-0" : "opacity-100 text-yellow-400"
         }`}
-        viewBox="0 0 17 17"
-        fill="currentColor"
-      >
-        <path d="M10 4.5a.75.75 0 01.75.75V6a.75.75 0 01-1.5 0v-.75A.75.75 0 0110 4.5zm0 8a.75.75 0 01.75.75V14a.75.75 0 01-1.5 0v-.75a.75.75 0 01.75-.75zm5.5-3.25a.75.75 0 010 1.5H14.75a.75.75 0 010-1.5h.75zm-8 0a.75.75 0 010 1.5H6a.75.75 0 010-1.5h.75zm6.364-3.114a.75.75 0 010 1.06l-.53.53a.75.75 0 01-1.06-1.06l.53-.53a.75.75 0 011.06 0zM7.226 12.774a.75.75 0 010 1.06l-.53.53a.75.75 0 11-1.06-1.06l.53-.53a.75.75 0 011.06 0zm6.138 1.06a.75.75 0 10-1.06-1.06l-.53.53a.75.75 0 101.06 1.06l.53-.53zM7.226 7.226a.75.75 0 10-1.06-1.06l-.53.53a.75.75 0 101.06 1.06l.53-.53zM10 7a3 3 0 100 6 3 3 0 000-6z" />
-      </svg>
+      />
 
       {/* ICONO LUNA */}
       <svg
