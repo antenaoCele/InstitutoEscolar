d7991a999349239a1cb3a15ae62673f20d814fb3
EXEQUIEL JOSE MENDOZA <exejm92@gmail.com>
2026-08-14 11:08:23 -0300

    Añade espacios en blanco para mayor claridad y organiza las opciones del parser.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,26 +6,37 @@ import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
+
   {
     files: ["**/*.{js,jsx}"],
+
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+
       parserOptions: {
         ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
+        ecmaFeatures: {
+          jsx: true,
+        },
         sourceType: "module",
       },
     },
+
     rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+
       "react-hooks/rules-of-hooks": "error",
+
       "react-hooks/exhaustive-deps": "warn",
+
+      "react-hooks/set-state-in-effect": "off",
     },
   },
 ]);
